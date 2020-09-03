fe6b2882a96b2804a01bca829c980db1031fbd16
Anurag842 <anuragdutta842@gmail.com>
2020-09-03 23:17:57 +0530

    First commit with stateless widget app

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+//runApp(App()); //runs the build method of App
+//}
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer");
+  }
+
+  @override //overrides the build method of statelesswidget
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite fruit?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TTRP'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions.elementAt(0)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print("answer2"),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => {print("answer3")},
+            ),
+          ],
+        ),
+      ), //home is a named argument and materialapp and text are both classes of flutter
+    );
+  }
+}
